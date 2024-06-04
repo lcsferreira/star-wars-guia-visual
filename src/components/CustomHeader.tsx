f1d8a02383dc8e4b4c93820fa8451138b82f22fd
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2024-06-04 11:46:33 -0300

    refactor: change header logic

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -6,18 +6,26 @@ type MenuItem = Required<MenuProps>["items"][number];
 const items: MenuItem[] = [
   {
     key: "1",
-    label: <Link to="/">Home</Link>,
-  },
-  {
-    key: "2",
-    label: <Link to="/characters">Characters</Link>,
+    label: (
+      <Link to="/">
+        <p
+          style={{
+            display: "flex",
+            alignItems: "center",
+            color: "white",
+            fontSize: "18px",
+            fontFamily: "SFDistantGalaxy-Regular",
+          }}
+        >
+          Star Wars
+        </p>
+      </Link>
+    ),
   },
 ];
 
 const CustomHeader = () => {
-  return (
-    <Menu mode="horizontal" defaultSelectedKeys={["1"]} items={items}></Menu>
-  );
+  return <Menu mode="horizontal" items={items}></Menu>;
 };
 
 export default CustomHeader;
